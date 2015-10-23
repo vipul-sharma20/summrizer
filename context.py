20a7eda6287611c1e85bd302528b09fa25f7db9d
Vipul Sharma <vipul.sharma20@gmail.com>
2015-10-24 01:44:31 +0530

    fixed backoff regexes

--- context.py
+++ context.py
@@ -4,13 +4,9 @@ from nltk.corpus import brown
 
 brown_train = brown.tagged_sents(categories='news')
 regexp_tagger = nltk.RegexpTagger(
-    [(r'^-?[0-9]+(.[0-9]+)?$', 'CD'),
-     (r'(-|:|;)$', ':'),
-     (r'\'*$', 'MD'),
-     (r'(The|the|A|a|An|an)$', 'AT'),
+     [(r'^[-\:]?[0-9]+(.[0-9]+)?$', 'CD'),
      (r'.*able$', 'JJ'),
      (r'^[A-Z].*$', 'NNP'),
-     (r'.*ness$', 'NN'),
      (r'.*ly$', 'RB'),
      (r'.*s$', 'NNS'),
      (r'.*ing$', 'VBG'),
